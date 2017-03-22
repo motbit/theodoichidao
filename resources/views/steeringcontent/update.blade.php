1acf50d8d5b08e7c938b8ab946262d5b24ad33b3
dtm273 <maimai273>
2017-03-22 15:03:43 +0700

    Update nguon chi dao

--- resources/views/steeringcontent/update.blade.php
+++ resources/views/steeringcontent/update.blade.php
@@ -18,44 +18,84 @@
     {!! Form::open(array('route' => 'steeringcontent-update', 'class' => 'form')) !!}
     {{ Form::hidden('id', $row->id, array('id' => 'id')) }}
 
-    <div class="form-group">
-        {!! Form::label('Nội dung chỉ đạo') !!}
+    <div class="form-group ">
+        <label>Tên nhiệm vụ:</label>
         {!! Form::textarea('content', $row->content,
             array('required',
                   'class'=>'form-control',
-                  'placeholder'=>'Nội dung chỉ đạo')) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('Thuộc kết luận') !!}
-        {!! Form::select('source', $source, $row->source,
-                array('no-required','class'=>'form-control')
-        ) !!}
+                  'placeholder'=>'Nội dung chỉ đạo',
+                  'rows'=>'2')) !!}
     </div>
 
-    <div class="form-group">
-        {!! Form::label('Đơn vị chủ trì') !!}
-        {!! Form::select('firtunit', $firstunit,$row->unit,
-                array('no-required','class'=>'form-control')
-        ) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('Đơn vị Phối hợp') !!}
-        <ul>
-            @foreach ($secondunit as $id=>$r1)
-                <li>{!! Form::checkbox('secondunit[]', $id, in_array($id,$dtfollow)) !!} {{ $r1 }}</li>
-            @endforeach
-        </ul>
-    </div>
     <div class="form-group form-inline">
-        {!! Form::label('Thời gian hoàn thành') !!}
-        {!! Form::text('deadline', $row->deadline,
+        <label>Nguồn chỉ đạo:</label>
+        {!! Form::text('source', $row->source,
+                array('no-required',
+                'placeholder'=>'Nguồn chỉ đạo',
+                'class'=>'form-control ipw', 'id'=>'source')
+        ) !!}
+        <div class="btn btn-default ico ico-search" data-toggle="modal" data-target="#modal-source"></div>
+    </div>
+
+    <div class="form-group form-inline">
+        <label>Người chỉ đạo:</label>
+        {!! Form::text('viphuman', $row->conductor,
+                array('no-required',
+                'placeholder'=>'Người chỉ đạo',
+                'class'=>'form-control ipw', 'id'=>'viphuman')
+        ) !!}
+        <div class="btn btn-default ico ico-search" data-toggle="modal" data-target="#modal-viphuman"></div>
+    </div>
+
+    <div class="form-group form-inline">
+        <label>Phân loại:</label>
+        @foreach($priority as $idx => $p)
+            <input type="radio" name="priority" value="{{$p->id}}" {{($idx == $row->priority)?'checked':''}}> {{$p->name}} &nbsp;&nbsp;&nbsp;&nbsp;
+        @endforeach
+    </div>
+
+    <div class="form-group form-inline">
+        <label>Đơn vị/Cá nhân chủ trì:</label>
+        <select id="fList" name="firtunit" class="form-control select-single ipw">
+            @foreach($treeunit as $item)
+                @foreach($item->children as $c)
+                    <option {{($row->unit == $c->id) ? 'selected' : ''}}  value="{{$c->id}}" >{{$c->name}}</option>
+                @endforeach
+            @endforeach
+        </select>
+        <div class="btn btn-default ico ico-search" data-toggle="modal" data-target="#firt-unit"></div>
+    </div>
+
+    <div class="form-group form-inline">
+        <label>Đơn vị/Cá nhân phối hợp:</label>
+        <select id="sList" name="secondunit[]" class="form-control select-multiple ipw" multiple="multiple">
+            @foreach($treeunit as $item)
+                @foreach($item->children as $c)
+                    <option value="{{$c->id}}" >{{$c->name}}</option>
+                @endforeach
+            @endforeach
+        </select>
+        <div class="btn btn-default ico ico-search" data-toggle="modal" data-target="#second-unit"></div>
+    </div>
+
+    <div class="form-group  form-inline">
+        <label>Thời hạn hoàn thành:</label>
+        {!! Form::text('deathline', $row->deadline,
             array('required',
                   'class'=>'form-control datepicker',
                   'placeholder'=>'Thời gian hoàn thành')) !!}
     </div>
 
+    <div class="form-group  form-inline">
+        <label>Ngày chỉ đạo:</label>
+        {!! Form::text('steer_time', $row->steer_time,
+            array('required',
+                  'class'=>'form-control datepicker',
+                  'placeholder'=>'Ngày bắt đầu')) !!}
+    </div>
+
     <div class="form-group">
-        {!! Form::label('Đơn vị Xác nhận') !!}
+        {!! Form::label('Đơn vị xác nhận:') !!}
         <ul>
             <li>{!! Form::radio('confirm', 'C',($row->xn=='C')) !!} Đơn vị chưa xác nhận</li>
             <li>{!! Form::radio('confirm', 'X',($row->xn=='X')) !!} Đơn vị đã xác nhận</li>
@@ -65,14 +105,12 @@
     </div>
 
     <div class="form-group">
-        {!! Form::label('Theo dõi của văn phòng') !!}
+        {!! Form::label('Theo dõi của văn phòng:') !!}
         {!! Form::textarea('note', $row->note,
             array('no-required',
                   'class'=>'form-control',
                   'placeholder'=>'Theo dõi của văn phòng')) !!}
     </div>
-
-
 
     <div class="form-group">
         {!! Form::label('Đánh giá') !!}
@@ -88,10 +126,224 @@
     </div>
     {!! Form::close() !!}
     @endforeach
+
+    <div id="modal-source" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Danh sách nguồn chỉ đạo:</h4>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-bordered">
+                        @foreach($sourcesteering as $s)
+                            <tr>
+                                <td><input type="radio" name="psource" class="pick-source" value="{{$s->code}}" data-time="{{date("d-m-Y", strtotime($s->time))}}"></td>
+                                <td>{{$s->code}}</td>
+                                <td>{{$s->name}}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="modal-viphuman" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Danh sách người chỉ đạo</h4>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-bordered">
+                        @foreach($viphuman as $v)
+                            <tr>
+                                <td><input type="radio" name="pviphuman" class="pick-source" value="{{$v->id}}"  data-name="{{$v->name}}"></td>
+                                <td>{{$v->name}}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="firt-unit" class="modal fade" role="dialog">
+        <div class="modal-dialog" style="min-width: 80%">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Danh sách đơn vị</h4>
+                </div>
+                <div class="modal-body">
+                    <ul class="nav nav-tabs">
+                        @foreach($treeunit as $idx=>$u)
+                            <li class="{{($idx == 0)?'active':''}}"><a data-toggle="tab"
+                                                                       href="#first-{{$u->id}}">{{$u->name}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+
+                    <div class="tab-content">
+                        @foreach($treeunit as $idx=>$u)
+                            <div id="first-{{$u->id}}" class="tab-pane fade in {{($idx == 0)?'active':''}}">
+                                <table class="table table-bordered">
+                                    <tr>
+                                        <th></th>
+                                        <th>Tên Ban - Đơn vị</th>
+                                        <th>Tên viết tắt</th>
+                                    </tr>
+                                    @foreach($u->children as $c)
+                                        <tr>
+                                            <td><input type="radio" name="pfunit" class="pick-firt-unit"
+                                                       value="{{$c->id}}"></td>
+                                            <td>{{$c->name}}</td>
+                                            <td>{{$c->shortname}}</td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="second-unit" class="modal fade" role="dialog">
+        <div class="modal-dialog" style="min-width: 80%">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Danh sách đơn vị</h4>
+                </div>
+                <div class="modal-body">
+                    <ul class="nav nav-tabs">
+                        @foreach($treeunit as $idx=>$u)
+                            <li class="{{($idx == 0)?'active':''}}"><a data-toggle="tab"
+                                                                       href="#sc-{{$u->id}}">{{$u->name}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+
+                    <div class="tab-content">
+                        @foreach($treeunit as $idx=>$u)
+                            <div id="sc-{{$u->id}}" class="tab-pane fade in {{($idx == 0)?'active':''}}">
+                                <table class="table table-bordered">
+                                    <tr>
+                                        <th></th>
+                                        <th>Tên Ban - Đơn vị</th>
+                                        <th>Tên viết tắt</th>
+                                    </tr>
+                                    @foreach($u->children as $c)
+                                        <tr>
+                                            <td><input type="checkbox" name="psunit" class="pick-firt-unit"
+                                                       value="{{$c->id}}"></td>
+                                            <td>{{$c->name}}</td>
+                                            <td>{{$c->shortname}}</td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="/js/jquery-ui.js"></script>
+    <link href="/css/jquery-ui.css" rel="stylesheet">
+
     <script>
+        var sources = [
+            @foreach($sourcesteering as $s)
+                    '{{$s->code}}',
+            @endforeach
+        ];
+        var viphumans = [
+            @foreach($viphuman as $v)
+                    '{{$v->name}}',
+            @endforeach
+        ];
+
+
         $( document ).ready(function() {
             // Handler for .ready() called.
-            $('.datepicker').datepicker({format: 'dd/mm/yyyy'});
+            $('.datepicker').datepicker({
+                format: 'dd-mm-yyyy',
+                dateFormat: 'dd-mm-yy',
+            });
+
+            $( "#source" ).autocomplete({
+                source: sources
+            });
+
+            $( "#viphuman" ).autocomplete({
+                source: viphumans
+            });
+
+            var arr = [{{$dtfollow}}];
+            $("#sList").val(arr).trigger('change');
         });
+
+        $('input:radio[name=psource]').change(function () {
+            $('input[name="source"]').val($('input[name="psource"]:checked').val())
+            var time = $('input[name="psource"]:checked').attr('data-time');
+            $('input[name="steer_time"]').val(time);
+        });
+
+        $('input[name="source"]').change(function() {
+            var val = $('input[name="source"]').val();
+            alert(val);
+            $('input:radio[name=psource][value="' + val + '"]').attr('checked',true);
+        });
+
+        $('input:radio[name=pviphuman]').change(function () {
+            var name = $('input[name="pviphuman"]:checked').attr('data-name');
+            $('input[name="viphuman"]').val(name);
+        });
+
+        $('input[name="viphuman"]').change(function() {
+            var val = $('input[name="viphuman"]').val();
+            $('input:radio[name=pviphuman][data-name="' + val + '"]').attr('checked',true);
+        });
+
+        $('input:radio[name=pfunit]').change(function () {
+            var id = $('input[name="pfunit"]:checked').val();
+            $('#fList option[value=' + id +']').attr('selected','selected');
+        });
+
+        $('input:checkbox[name=psunit]').change(function () {
+            var arr = [];
+            var vl = '';
+            $('input:checkbox[name=psunit]:checked').each(function(){
+                vl = $(this).val();
+                arr.push(vl);
+            });
+            $("#sList").val(arr).trigger('change');
+        });
+
+        $('#fList').change(function() {
+            var val = $("#fList option:selected").val();
+            $('input:radio[name=pfunit][value="' + val + '"]').attr('checked',true);
+        });
+
+        $('#sList').on("select2:select", function(event) {
+            $(event.currentTarget).find("option:selected").each(function(i, selected){
+                i = $(selected).val();
+                $('input:checkbox[name=psunit][value="' + i + '"]').attr('checked',true);
+            });
+        });
+
+        $("#sList").on("select2:unselect", function (event) {
+            $('input:checkbox[name=psunit]').attr('checked',false);
+
+            $(event.currentTarget).find("option:selected").each(function(i, selected){
+                i = $(selected).val();
+                $('input:checkbox[name=psunit][value="' + i + '"]').attr('checked',true);
+            });
+        });
+
+        $(".select-multiple").select2();
+        $(".select-single").select2();
     </script>
 @stop
