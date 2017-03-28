d64d28c935517d5a22d67b765a13918624280682
TK EDU <motbit.com@gmail.com>
2017-03-28 15:22:06 +0700

    Error Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,9 @@
-@extends('layout1')
-<h2>{{ $exception->getMessage() }}</h2>
+@extends('layouts.app')
+@section('page-title')
+    Trang yêu cầu không tồn tại!
+@stop
+@section('content')
+<h2>Trang yêu cầu không tồn tại!</h2>
+<h4>{{ $exception->getMessage() }}</h4>
+@stop
 
